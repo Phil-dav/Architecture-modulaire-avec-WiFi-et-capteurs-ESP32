54c51375b9a9280dafeead6c8b4c7b7a0f6a7413
Phil-dav <flophil34@yahoo.com>
2025-08-26 11:47:33 +0200

    modif mise a jour certain fichiers

---
+++ include/pcf8574_config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Mapping des broches PCF8574
+#define PCF_PIN_RELAY_1       0
+#define PCF_PIN_RELAY_2       1
+#define PCF_PIN_RELAY_3       2
+#define PCF_PIN_RELAY_4       3
+#define PCF_PIN_BP_SCREEN     4
+#define PCF_PIN_MODE_AUTO     5
+#define PCF_PIN_MODE_MANU     6
+#define PCF_PIN_DEFAUT_MOTEUR 7
+
--- include/secrets.h
+++ include/secrets.h
@@ -1,5 +1,5 @@
 // secrets.h
 #pragma once
 
-#define MON_SSID "TP-Link_Extender"
-#define MON_MDP  "123456789"
+#define WIFI_SSID "TP-Link_Extender"
+#define WIFI_PASSWORD  "123456789"
